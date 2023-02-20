edbab4f4f5c3c6f10e1610b241f922c34fe4af7b
Precious <precious.akpan2000@gmail.com>
2023-02-20 09:41:53 +0100

    search views impl

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,13 +1,48 @@
-const recipeContainer = document.querySelector('.recipe');
+import * as model from './model';
+import recipeView from './views/recipeView';
+import 'core-js';
+import 'regenerator-runtime/runtime';
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
+// const recipeContainer = document.querySelector('.recipe');
+
+
+
 
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+
+// controlRecipes();
+
+
+const controlRecipes = async () => {
+  try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+    if (!id) return;
+    recipeView.renderSpinner();
+    //fetching recipe
+    await model.loadRecipe(id)
+
+    //rendering recipe
+    recipeView.render(model.state.recipe)
+
+  } catch (e) {
+    recipeView.renderError();
+  }
+};
+
+//controlSearch()
+
+const contronSearch = async function() {
+  await model.loadSearch('pizza')
+  console.log(model.state.search.results);
+}
+
+contronSearch()
+const init = function() {
+  recipeView.addHandleRender(controlRecipes)
+}
+
+init()
---
+++ src/js/model.js
@@ -0,0 +1,51 @@
+import { API_URL } from './config';
+import { getJSON } from './helper';
+
+export const state = {
+  recipe: {},
+  search: {
+    query: '',
+    results: []
+  }
+};
+
+export const loadRecipe = async function(id) {
+
+  try {
+
+    const data = await getJSON(`${API_URL}${id}`);
+
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients
+    };
+    console.log(state.recipe);
+  } catch (e) {
+    console.error(`${e} 💥💥💥`);
+    throw e;
+  }
+};
+
+export const loadSearch = async function(query) {
+  try {
+    state.search.query = query;
+    const data = await getJSON(`${API_URL}?search=${query}`);
+    state.search.results = data.data.recipes.map(recipe => {
+     return {id: recipe.id,
+        title: recipe.title,
+        publisher: recipe.publisher,
+        image: recipe.image_url,}
+    })
+  } catch (e) {
+    console.error(`${e} 💥💥💥`);
+    throw e;
+  }
+};
+
